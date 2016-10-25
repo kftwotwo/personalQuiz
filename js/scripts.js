9fa5eed6c4aa110eb8b69b7ea6e7b634229f0938
jx and kf <jx@>
2016-10-25 15:12:44 -0700

    add stress test and alert prompt

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,26 @@ $(document).ready(function(){
   $("form#ailments").submit(function(event){
     event.preventDefault();
     $("#body-responses").show();
+       totalSick = [];
     $("input:checkbox[name=sick]:checked").each(function(){
       var sickMode = $(this).val();
-      $('#body-responses').append(sickMode + "<br>");
+          totalSick.push(sickMode);
+       numSicks = totalSick.length;
     });
+
     $("#life-responses").show();
+     totalLife = [];
     $("input:checkbox[name=life]:checked").each(function(){
       var lifeMode = $(this).val();
-      $('#life-responses').append(lifeMode + "<br>");
+        totalLife.push(lifeMode);
+       numLifes = totalLife.length;
     });
+    if (numSicks > 4 || numLifes >4 )
+      alert("Please seek medical attention!");
+      else {
+        alert("Please drink more water!");
+      }
+
     $('#ailments').hide();
   });
 });
